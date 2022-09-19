52d04ca912a7f82cc40522fab1034d61dde0853b
dwisabrina23 <dwisabrina10@gmail.com>
2022-09-19 23:00:17 +0800

    feat(api): add custom hook for playlist

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable new-cap */
 import NextImage from 'next/image'
 import NextLink from 'next/link'
+
 import {
   Box,
   List,
@@ -17,6 +18,8 @@ import {
   MdPlaylistAdd,
   MdFavorite,
 } from 'react-icons/md'
+
+import { usePlaylist } from '../lib/hooks'
 
 const navMenu = [
   {
@@ -49,8 +52,10 @@ const musicMenu = [
   },
 ]
 
-const playList = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
+// const playList = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`)
 const Sidebar = () => {
+  const { playlists } = usePlaylist()
+
   return (
     <Box
       width="100%"
@@ -106,11 +111,11 @@ const Sidebar = () => {
         <Divider bg="gray.900" />
         <Box height="60%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
-            {playList.map((playlist) => (
-              <ListItem paddingX="20px" key={playlist}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist.id}>
                 <LinkBox>
                   <NextLink href="/" passHref>
-                    <LinkOverlay>{playlist}</LinkOverlay>
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
